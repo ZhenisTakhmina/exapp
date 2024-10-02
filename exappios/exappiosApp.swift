2a28342639cbbbce73bad6d4f51a6f42b48a35e7
ZhenisTakhmina <zhenistakhmina@gmail.com>
2024-10-02 16:54:58 +0500

    changes

--- exappios/exappiosApp.swift
+++ exappios/exappiosApp.swift
@@ -18,16 +18,14 @@ struct exappiosApp: App {
         WindowGroup {
             @AppStorage("onboarding") var onboarding = true
             let userDefaultsManager = UserDefaultsManager.shared
-
-            OnboardingView()
-//            if onboarding {
-//                OnboardingView()
-//            } else {
-//                NavigationStack {
-//                    ChatView(style: ChatStyle(rawValue: userDefaultsManager.savedStyle) ?? .telegram, header: ChatHeader(title: userDefaultsManager.savedExName , subtitle: "был(а) недавно", avatarImage: Image(userDefaultsManager.savedAvatar) ))
-//                }
-//            }
             
+            if onboarding {
+                OnboardingView()
+            } else {
+                NavigationStack {
+                    ChatView(style: ChatStyle(rawValue: userDefaultsManager.savedStyle) ?? .telegram, header: ChatHeader(title: userDefaultsManager.savedExName , subtitle: "был(а) недавно", avatarImage: Image(userDefaultsManager.savedAvatar) ))
+                }
+            }
         }
     }
 }
